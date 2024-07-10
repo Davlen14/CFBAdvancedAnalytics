ff671c9967629397cb36ae258e7fd7450c70af0c
Davlen14 <99432647+Davlen14@users.noreply.github.com>
2024-07-09 21:52:27 -0500

    Added responsive navbar with hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TeamsComponent from './components/TeamsComponent';
@@ -8,19 +8,28 @@ import ThemeToggle from './components/ThemeToggle';
 import HomeComponent from './components/HomeComponent';
 
 function App() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <Router>
       <div className="navbar">
         <div className="navbar-content">
           <div className="title">NCAAF</div>
-          <div className="nav-buttons">
+          <button className="hamburger-menu" onClick={toggleMobileMenu}>
+            &#9776;
+          </button>
+          <div className={`nav-buttons ${isMobileMenuOpen ? 'active' : ''}`}>
             <Link to="/" className="nav-button">Home</Link>
             <Link to="/projections" className="nav-button">Projections</Link>
             <Link to="/teams" className="nav-button">Teams</Link>
             <Link to="/games" className="nav-button">Games</Link>
+            <ThemeToggle />
           </div>
         </div>
-        <ThemeToggle />
       </div>
 
       <Routes>
@@ -34,6 +43,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
