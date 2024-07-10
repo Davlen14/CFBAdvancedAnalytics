f0fec7cce11b7268aa1a9aee5760a298d459b35c
Davlen14 <99432647+Davlen14@users.noreply.github.com>
2024-07-10 00:37:02 -0500

    Update navbar to include search bar and modern design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TeamsComponent from './components/TeamsComponent';
@@ -8,28 +8,22 @@ import ThemeToggle from './components/ThemeToggle';
 import HomeComponent from './components/HomeComponent';
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
     <Router>
       <div className="navbar">
         <div className="navbar-content">
-          <div className="title">NCAAF</div>
-          <button className="hamburger-menu" onClick={toggleMenu}>
-            &#9776;
-          </button>
-          <div className={`nav-buttons ${menuOpen ? 'active' : ''}`}>
-            <Link to="/" className="nav-button" onClick={toggleMenu}>Home</Link>
-            <Link to="/projections" className="nav-button" onClick={toggleMenu}>Projections</Link>
-            <Link to="/teams" className="nav-button" onClick={toggleMenu}>Teams</Link>
-            <Link to="/games" className="nav-button" onClick={toggleMenu}>Games</Link>
-            <ThemeToggle />
+          <div className="title">CFB Data Vault</div>
+          <div className="search-bar">
+            <input type="text" placeholder="Search..." />
+          </div>
+          <div className="nav-buttons">
+            <Link to="/" className="nav-button">Home</Link>
+            <Link to="/projections" className="nav-button">Projections</Link>
+            <Link to="/teams" className="nav-button">Teams</Link>
+            <Link to="/games" className="nav-button">Games</Link>
           </div>
         </div>
+        <ThemeToggle />
       </div>
 
       <Routes>
@@ -43,6 +37,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
