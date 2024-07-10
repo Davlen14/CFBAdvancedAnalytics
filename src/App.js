392acf4d7a2797f8ac6dbebd70172ff9cbf481c8
Davlen14 <99432647+Davlen14@users.noreply.github.com>
2024-07-10 00:21:22 -0500

    Add hamburger menu with theme toggle button for mobile view

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import TeamsComponent from './components/TeamsComponent';
 import UpcomingGames from './components/UpcomingGames';
 import ThemeToggle from './components/ThemeToggle';
 import HomeComponent from './components/HomeComponent';
-import { ReactComponent as HomeIcon } from './icons/home.svg';
-import { ReactComponent as ProjectionsIcon } from './icons/projections.svg';
-import { ReactComponent as TeamsIcon } from './icons/teams.svg';
-import { ReactComponent as GamesIcon } from './icons/games.svg';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,26 +18,15 @@ function App() {
     <Router>
       <div className="navbar">
         <div className="navbar-content">
-          <div className="title">
-            <img src="/logo.png" alt="CFB Data Vault Logo" />
-            NCAAF
-          </div>
+          <div className="title">NCAAF</div>
           <button className="hamburger-menu" onClick={toggleMenu}>
             &#9776;
           </button>
           <div className={`nav-buttons ${menuOpen ? 'active' : ''}`}>
-            <Link to="/" className="nav-button" onClick={toggleMenu}>
-              <HomeIcon /> Home
-            </Link>
-            <Link to="/projections" className="nav-button" onClick={toggleMenu}>
-              <ProjectionsIcon /> Projections
-            </Link>
-            <Link to="/teams" className="nav-button" onClick={toggleMenu}>
-              <TeamsIcon /> Teams
-            </Link>
-            <Link to="/games" className="nav-button" onClick={toggleMenu}>
-              <GamesIcon /> Games
-            </Link>
+            <Link to="/" className="nav-button" onClick={toggleMenu}>Home</Link>
+            <Link to="/projections" className="nav-button" onClick={toggleMenu}>Projections</Link>
+            <Link to="/teams" className="nav-button" onClick={toggleMenu}>Teams</Link>
+            <Link to="/games" className="nav-button" onClick={toggleMenu}>Games</Link>
             <ThemeToggle />
           </div>
         </div>
@@ -58,7 +43,6 @@ function App() {
 }
 
 export default App;
-
 
 
 
