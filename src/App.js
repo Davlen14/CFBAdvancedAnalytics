543fcf6a9048c9ce7fa965ea179e890de24df054
Davlen14 <99432647+Davlen14@users.noreply.github.com>
2024-07-11 13:38:35 -0500

    Update navbar and add GameDay logo1 two

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TeamsComponent from './components/TeamsComponent';
 import UpcomingGames from './components/UpcomingGames';
 import ThemeToggle from './components/ThemeToggle';
 import HomeComponent from './components/HomeComponent';
 import MetricsComponent from './components/MetricsComponent'; // Import MetricsComponent
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import logo from '/Users/davlenswain/my-betting-bot/public/Game Day.png'; // Correct path
+import logo from './assets/Game Day.png'; // Updated path to the logo
+import '../App.css'; // Ensure this path is correct
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,7 +20,7 @@ function App() {
     <Router>
       <div className="navbar">
         <div className="navbar-content">
-          <img src={logo} alt="GameDay Logo" className="logo" />
+          <img src={logo} alt="GDA Logo" className="logo" />
           <button className="hamburger-menu" onClick={toggleMenu}>
             &#9776;
           </button>
@@ -31,9 +29,7 @@ function App() {
             <Link to="/metrics" className="nav-button" onClick={toggleMenu}>Metrics</Link>
             <Link to="/teams" className="nav-button" onClick={toggleMenu}>Teams</Link>
             <Link to="/games" className="nav-button" onClick={toggleMenu}>Games</Link>
-            <button className="theme-toggle-button" onClick={ThemeToggle}>
-              <FontAwesomeIcon icon={faMoon} />
-            </button>
+            <ThemeToggle />
           </div>
         </div>
       </div>
@@ -49,9 +45,6 @@ function App() {
 }
 
 export default App;
-
-
-
 
 
 
