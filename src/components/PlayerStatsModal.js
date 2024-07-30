c4a57383fc61832eb93874846417dd7908ea5d96
Davlen14 <99432647+Davlen14@users.noreply.github.com>
2024-07-30 15:34:21 -0500

    Implement player season stats modal and integrate with UpcomingGamesComponent

--- src/components/PlayerStatsModal.js
+++ src/components/PlayerStatsModal.js
@@ -2,6 +2,15 @@ import React from 'react';
 import '../App.css';
 
 const PlayerStatsModal = ({ game, playerSeasonStats, closeModal }) => {
+  // Function to get the team logo based on the team name
+  const getTeamLogo = (teamName) => {
+    const teamLogosMap = {
+      [game.home_team.school]: game.homeTeamLogo,
+      [game.away_team.school]: game.awayTeamLogo,
+    };
+    return teamLogosMap[teamName];
+  };
+
   return (
     <div className="player-modal">
       <div className="player-modal-content">
@@ -11,6 +20,11 @@ const PlayerStatsModal = ({ game, playerSeasonStats, closeModal }) => {
           {playerSeasonStats.length > 0 ? (
             playerSeasonStats.map((stat, index) => (
               <div key={index} className="player-modal-stat-entry">
+                <img
+                  src={getTeamLogo(stat.team)}
+                  alt={`${stat.team} logo`}
+                  className="player-modal-team-logo"
+                />
                 <h3>{stat.player}</h3>
                 <p>{stat.statType}: {stat.stat}</p>
               </div>
@@ -25,4 +39,5 @@ const PlayerStatsModal = ({ game, playerSeasonStats, closeModal }) => {
 };
 
 export default PlayerStatsModal;
+
 
